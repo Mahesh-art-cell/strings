d38d4ccefc58246c562f8d24d77e5e842ad1371d
Mahesh-art-cell <tallurim886@email.com>
2024-09-25 21:43:36 +0530

    all files are added

--- day5.js
+++ day5.js
@@ -1,3 +1,6 @@
+//Practice
+//--------------------------
+
 // string lengh
 var a="mahesh";
 console.log(a.length);
@@ -50,5 +53,42 @@ console.log(h[h.length-6]);
 // string at()
 var i="mahesh";
 console.log(i.at(0));
+//-------------------------------------
 
+//Assignements
+//-------------------------------------
+//Task-1
+var str1 = "Mahesh       ";
+var str2 = "         msd";
+var res=str1.trimEnd().concat(" ",str2.trimStart()).toUpperCase();
+console.log(res);
 
+//Task-2
+var s = "i am TALLURI Mahesh from ktp";
+var res = s.slice(5, 19).toLowerCase(),r=s.charAt(18);
+console.log(res,r);
+
+//Task-3
+var c="M";
+var r=c.concat(" ","Talluri").toUpperCase(),r1=r[r.length-2]
+console.log(r,r1);
+
+//Task-4
+var s1="murthy";
+console.log(s1);
+var s2="koteswaramma";
+console.log(s2);
+var r=(s1[0].toUpperCase()+s1.slice(1,3)).concat(s2.slice(-3,-1)+s2[s2.length-1].toUpperCase());
+console.log(r);
+
+//Task-5
+var st="      mahesh talluri   ";
+var st1="msd"
+var r=(st.trim().charAt(0).toUpperCase()+st.trim().slice(1,6)).concat(" ",st1.slice(0,2)+st1.charAt(2).toUpperCase());
+console.log(r);
+
+//Task-6
+var s="hello there , how are you";
+console.log(s);
+var r=s.indexOf('are');
+console.log(r);
--- task6.js
+++
@@ -1,4 +0,0 @@
-var s="hello there , how are you";
-console.log(s);
-var r=s.indexOf('are');
-console.log(r);
